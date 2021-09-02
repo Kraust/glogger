be3cd7642a5ab001bf69874f40b76c57018dbc8c
Kraust <kraust@cock.li>
2021-09-02 11:28:23 -0400

    Nice vector over.

--- glogger.go
+++ glogger.go
@@ -2,6 +2,8 @@ package glogger
 
 import (
 	"fmt"
+	"log"
+	"log/syslog"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -11,47 +13,93 @@ import (
 type GloggerLevel string
 
 const (
-	GloggerLevelInfo    = "INFO"
-	GloggerLevelError   = "ERROR"
-	GloggerLevelWarning = "WARNING"
-	GloggerLevelFatal   = "FATAL"
+	LevelInfo    = "INFO"
+	LevelError   = "ERROR"
+	LevelWarning = "WARNING"
+	LevelFatal   = "FATAL"
 )
 
 type Glogger struct {
-	Levels []GloggerLevel
+	Levels    []GloggerLevel
+	UseSyslog bool
 }
 
-// LEVEL: file:line:function():
-func (ctx *Glogger) glog(level string, format string, args ...interface{}) {
+func (g *Glogger) glog(level GloggerLevel, format string, args ...interface{}) {
 	pc, fn, ln, _ := runtime.Caller(1)
 	name := strings.Split(runtime.FuncForPC(pc).Name(), ".")
+	appname := filepath.Base(os.Args[0])
 
-	f := fmt.Sprintf("%s: %s:%d:%s(): %s", level, filepath.Base(fn), ln, name[len(name)-1], format)
-	fmt.Println(fmt.Sprintf(f, args...))
+	for _, l := range g.Levels {
+		if l == level {
+			f := fmt.Sprintf("[%s] %s:%d:%s(): %s", level, filepath.Base(fn),
+				ln, name[len(name)-1], format)
+			if g.UseSyslog {
+				lw, err := syslog.New(syslog.LOG_INFO, appname)
+				if err == nil {
+					log.SetOutput(lw)
+					log.Print(fmt.Sprintf(f, args...))
+				}
+			} else {
+				log.Println(fmt.Sprintf(f, args...))
+			}
+		}
+	}
 }
 
+func (g *Glogger) Printf(format string, args ...interface{}) {
+	g.glog(LevelInfo, format, args...)
+}
+
+func (g *Glogger) Infof(format string, args ...interface{}) {
+	g.glog(LevelInfo, format, args...)
+}
+
+func (g *Glogger) Errorf(format string, args ...interface{}) {
+	g.glog(LevelError, format, args...)
+}
+
+func (g *Glogger) Warningf(format string, args ...interface{}) {
+	g.glog(LevelWarning, format, args...)
+}
+
+func (g *Glogger) Fatalf(format string, args ...interface{}) {
+	g.glog(LevelFatal, format, args...)
+	os.Exit(1)
+}
+
+/* These do not need a context */
 func Printf(format string, args ...interface{}) {
-	g := Glogger{}
-	g.glog("INFO", format, args...)
+	g := Glogger{
+		Levels: []GloggerLevel{LevelInfo},
+	}
+	g.glog(LevelInfo, format, args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	g := Glogger{}
-	g.glog("INFO", format, args...)
+	g := Glogger{
+		Levels: []GloggerLevel{LevelInfo},
+	}
+	g.glog(LevelInfo, format, args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	g := Glogger{}
-	g.glog("ERROR", format, args...)
+	g := Glogger{
+		Levels: []GloggerLevel{LevelError},
+	}
+	g.glog(LevelError, format, args...)
 }
 
 func Warningf(format string, args ...interface{}) {
-	g := Glogger{}
-	g.glog("WARN", format, args...)
+	g := Glogger{
+		Levels: []GloggerLevel{LevelWarning},
+	}
+	g.glog(LevelWarning, format, args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	g := Glogger{}
-	g.glog("FATAL", format, args...)
+	g := Glogger{
+		Levels: []GloggerLevel{LevelFatal},
+	}
+	g.glog(LevelFatal, format, args...)
 	os.Exit(1)
 }
--- glogger_test.go
+++ glogger_test.go
@@ -1,6 +1,7 @@
 package glogger
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -9,5 +10,28 @@ func TestGlog(t *testing.T) {
 	Infof("this is Infof")
 	Errorf("this is Errorf")
 	Warningf("this is Warningf")
-	Fatalf("this is Fatalf")
+	// Fatalf("this is Fatalf")
+
+	fmt.Println("====================")
+	g := Glogger{
+		Levels: []GloggerLevel{
+			LevelInfo,
+			LevelError,
+			LevelWarning,
+			LevelFatal,
+		},
+	}
+	g.Printf("this is Printf with ctx")
+	g.Infof("this is Infof with ctx")
+	g.Errorf("this is Errorf with ctx")
+	g.Warningf("this is Warningf with ctx")
+	// g.Fatalf("this is Fatalf with ctx")
+
+	g.UseSyslog = true
+	g.Printf("this is Printf with ctx")
+	g.Infof("this is Infof with ctx")
+	g.Errorf("this is Errorf with ctx")
+	g.Warningf("this is Warningf with ctx")
+	// g.Fatalf("this is Fatalf with ctx")
+
 }
